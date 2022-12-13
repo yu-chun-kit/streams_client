fad5257ba27460c5b142242205e040ae89849bd3
yu-chun-kit <johnson37siumen@hotmail.com>
2022-12-14 01:08:56 +0800

    add google api method

--- src/components/GoogleAuth.tsx
+++ src/components/GoogleAuth.tsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useEffect } from "react";
 
-type Props = {}
+type Props = {};
 
 const GoogleAuth = (props: Props) => {
-  return (
-    <div>GoogleAuth</div>
-  )
-}
+  useEffect(() => {
+    window!.gapi.load("client:auth2", () => {
+      window!.gapi.client.init({
+        clientId: "YOUR_CLIENT_ID",
+        scope: "email",
+        plugin_name: "streamy",
+      });
+    });
+  }, []);
+
+  return <div>GoogleAuth</div>;
+};
 
 export default GoogleAuth;
---
+++ src/types/index.d.ts
@@ -0,0 +1,7 @@
+export {};
+declare global {
+  interface Window {
+    gapi: any;
+  }
+}
+window.gapi = window.gapi || {};
