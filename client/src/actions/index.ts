f7abf191b47e96cf38839dcc5143fb672dc5ed3d
yu-chun-kit <johnson37siumen@hotmail.com>
2023-02-13 02:24:29 +0800

    add type and redux-thunk actions

--- client/src/actions/index.ts
+++ client/src/actions/index.ts
@@ -1,5 +1,13 @@
 import streams from "../apis/streams";
-import { SIGN_IN, SIGN_OUT } from "./types";
+import {
+  SIGN_IN,
+  SIGN_OUT,
+  CREATE_STREAM,
+  FETCH_STREAMS,
+  FETCH_STREAM,
+  DELETE_STREAM,
+  EDIT_STREAM,
+} from "./types";
 
 export const signIn = (userId: any) => {
   return {
@@ -15,5 +23,31 @@ export const signOut = () => {
 };
 
 export const createStream = (formValues: any) => async (dispatch: any) => {
-  streams.post("/streams", formValues);
+  const response = await streams.post("/streams", formValues);
+
+  dispatch({ type: CREATE_STREAM, payload: response.data });
 };
+
+export const fetchStreams = () => async (dispatch: any) => {
+  const response = await streams.get("/streams");
+
+  dispatch({ type: FETCH_STREAMS, payload: response.data });
+};
+
+export const fetchStream = (id: number) => async (dispatch: any) => {
+  const response = await streams.get("/streams/${id}");
+
+  dispatch({ type: FETCH_STREAM, payload: response.data });
+}
+
+const deleteStream = (id: number) => async (dispatch: any) => {
+  await streams.delete("/streams/${id}");
+
+  dispatch({ type: DELETE_STREAM, payload: id });
+}
+
+const editStream = (id: number, formValues: any) => async (dispatch: any) => {
+  const response = await streams.put("/streams/${id}", formValues);
+
+  dispatch({ type: EDIT_STREAM, payload: response.data });
+}
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,12 +4,13 @@ import { Provider } from "react-redux";
 import { createStore, applyMiddleware, compose } from "@reduxjs/toolkit";
 // import reducers from "./reducers";
 // import { createStore, applyMiddleware, compose } from "redux";
+import reduxThunk from "redux-thunk";
 import "semantic-ui-css/semantic.min.css";
 import reducers from "./reducers";
 
 const composeEnhancers =
   (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducers, composeEnhancers(applyMiddleware()));
+const store = createStore(reducers, composeEnhancers(applyMiddleware(reduxThunk)));
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
